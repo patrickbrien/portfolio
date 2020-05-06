ea5ffb4f79042de04797859a6d214b7bcc5e6463
Daniel Courtness <danielcourtness@gmail.com>
2020-05-06 21:01:53 +1000

    Lock viewport height

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,6 +2,7 @@
 
 import React, { Component, useContext } from "react";
 import { graphql } from "gatsby";
+import Div100vh from "react-div-100vh";
 import Img from "gatsby-image";
 import { AppContext } from "~context/AppContext";
 import { CursorContext } from "~context/CursorContext";
@@ -70,112 +71,114 @@ class IndexPageComponent extends Component {
         />
 
         <Layout className="index-page w-full h-screen relative">
-          <div className="h-full grid">
-            <nav className="h-full sm:h-auto grid-end-5 sm:grid-end-12 relative flex flex-col justify-between pt-8 pb-12">
-              <article>
-                <h1 className="f1">Patrick Brien</h1>
-                <h2 className="f1">Digital Branding</h2>
-                <h2 className="f1">and Website Design</h2>
+          <Div100vh className="relative overflow-hidden">
+            <div className="h-full grid">
+              <nav className="h-full sm:h-auto grid-end-5 sm:grid-end-12 relative flex flex-col justify-between pt-8 pb-12">
+                <article>
+                  <h1 className="f1">Patrick Brien</h1>
+                  <h2 className="f1">Digital Branding</h2>
+                  <h2 className="f1">and Website Design</h2>
 
-                <h2 className="mt-16 sm:mt-12 f1">
-                  Currently learning: {currentlyLearning}
-                </h2>
-              </article>
+                  <h2 className="mt-16 sm:mt-12 f1">
+                    Currently learning: {currentlyLearning}
+                  </h2>
+                </article>
 
-              <ul className="index-page__links index-page__links--desktop flex">
-                <li className="relative mr-1 f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="mailto:hello@patrickbrien.com.au"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Email,
-                  </a>
-                </li>
+                <ul className="index-page__links index-page__links--desktop flex">
+                  <li className="relative mr-1 f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="mailto:hello@patrickbrien.com.au"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Email,
+                    </a>
+                  </li>
 
-                <li className="relative mr-1 f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="https://www.instagram.com/patrickbrien/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Instagram,
-                  </a>
-                </li>
+                  <li className="relative mr-1 f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="https://www.instagram.com/patrickbrien/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Instagram,
+                    </a>
+                  </li>
 
-                <li className="relative f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="https://open.spotify.com/user/pbrien1?si=wykLl5prRmqAc96mUbUXGw"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Spotify
-                  </a>
-                </li>
-              </ul>
-            </nav>
+                  <li className="relative f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="https://open.spotify.com/user/pbrien1?si=wykLl5prRmqAc96mUbUXGw"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Spotify
+                    </a>
+                  </li>
+                </ul>
+              </nav>
 
-            <section className="grid-end-7 sm:grid-end-12 relative flex items-end pb-12 sm:pb-16">
-              <div
-                className="index-page__carousel w-full relative overflow-hidden"
-                // style={{ transform: imageTranslate }}
-              >
-                {activeImage && (
-                  <Img
-                    className="animation-fade-in-slow w-full absolute transform-center z-10"
-                    fluid={activeImage.childImageSharp.fluid}
-                    alt="Carousel Image Active"
-                  />
-                )}
+              <section className="grid-end-7 sm:grid-end-12 relative flex items-end pb-12 sm:pb-16">
+                <div
+                  className="index-page__carousel w-full relative overflow-hidden"
+                  // style={{ transform: imageTranslate }}
+                >
+                  {activeImage && (
+                    <Img
+                      className="animation-fade-in-slow w-full absolute transform-center z-10"
+                      fluid={activeImage.childImageSharp.fluid}
+                      alt="Carousel Image Active"
+                    />
+                  )}
 
-                {nextImage && (
-                  <Img
-                    className="animation-fade-in-slow animation-delay-1 w-full absolute transform-center"
-                    fluid={activeImage.childImageSharp.fluid}
-                    alt="Carousel Image Cached"
-                  />
-                )}
-              </div>
+                  {nextImage && (
+                    <Img
+                      className="animation-fade-in-slow animation-delay-1 w-full absolute transform-center"
+                      fluid={activeImage.childImageSharp.fluid}
+                      alt="Carousel Image Cached"
+                    />
+                  )}
+                </div>
 
-              <ul className="index-page__links index-page__links--touch absolute bottom-0 right-0 left-0 pb-4 flex">
-                <li className="relative mr-1 f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="mailto:hello@patrickbrien.com.au"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Email,
-                  </a>
-                </li>
+                <ul className="index-page__links index-page__links--touch absolute bottom-0 right-0 left-0 pb-4 flex">
+                  <li className="relative mr-1 f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="mailto:hello@patrickbrien.com.au"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Email,
+                    </a>
+                  </li>
 
-                <li className="relative mr-1 f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="https://www.instagram.com/patrickbrien/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Instagram,
-                  </a>
-                </li>
+                  <li className="relative mr-1 f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="https://www.instagram.com/patrickbrien/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Instagram,
+                    </a>
+                  </li>
 
-                <li className="relative f1 text-black">
-                  <a
-                    className="hover-underline"
-                    href="https://open.spotify.com/user/pbrien1?si=wykLl5prRmqAc96mUbUXGw"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Spotify
-                  </a>
-                </li>
-              </ul>
-            </section>
-          </div>
+                  <li className="relative f1 text-black">
+                    <a
+                      className="hover-underline"
+                      href="https://open.spotify.com/user/pbrien1?si=wykLl5prRmqAc96mUbUXGw"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Spotify
+                    </a>
+                  </li>
+                </ul>
+              </section>
+            </div>
+          </Div100vh>
         </Layout>
       </>
     );
